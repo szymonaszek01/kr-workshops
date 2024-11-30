02aab33e27cac1c0e54378c75abf4a9cee35cf30
szymonaszek01 <235879@edu.p.lodz.pl>
2024-11-30 15:20:18 +0100

    update eslint config

--- backend-app/eslint.config.mjs
+++ backend-app/eslint.config.mjs
@@ -1,10 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "n/no-missing-import": "error", // Prevent missing imports
+      "n/no-deprecated-api": "warn", // Warn about deprecated APIs
+      "n/no-unpublished-require": "error", // Avoid using dependencies not in `package.json`
+      "no-console": "off", // Allow `console.log` (useful in Node.js apps)
+      strict: ["error", "global"], // Enforce global strict mode
+    },
+  },
 ];
