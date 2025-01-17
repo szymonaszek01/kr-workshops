676fb47c3e1ef31221099c04f88f8865fe2db362
klarutkow <86961229+klarutkow@users.noreply.github.com>
2025-01-17 15:23:13 +0100

    Form validation

--- frontend-app/src/app/components/auth-form/auth-form.component.ts
+++ frontend-app/src/app/components/auth-form/auth-form.component.ts
@@ -18,7 +18,7 @@ import { MatCardModule } from '@angular/material/card';
 export class AuthFormComponent {
   loginForm = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required],
+      validators: [Validators.required, Validators.email],
     }),
     password: new FormControl('', {
       validators: [Validators.required],
@@ -27,7 +27,7 @@ export class AuthFormComponent {
 
   isEmailInvalid(): boolean | undefined {
     console.log(this.loginForm.get('email')?.errors);
-    return this.loginForm.get('email')?.touched && this.loginForm.get('email')?.invalid;
+    return this.loginForm.get('email')?.invalid;
   }
 
   getEmailErrorMessage(): string {
@@ -35,16 +35,32 @@ export class AuthFormComponent {
     if (!errors) {
       return '';
     }
-    return errors['required'] ? 'Email is required' : 'Email has not a vaild structure'; 
+    return errors['required']
+      ? 'Email is required'
+      : 'Email has not a vaild structure';
   }
 
-  isPasswordInvalid() {
+  isPasswordInvalid(): boolean | undefined {
     console.log(this.loginForm.get('password')?.errors);
-    return this.loginForm.get('password')?.touched && this.loginForm.get('password')?.invalid;
+    return this.loginForm.get('password')?.invalid;
+  }
+
+  getPasswordErrorMessage(): string {
+    const errors = this.loginForm.get('password')?.errors;
+    if (!errors) {
+      return '';
+    }
+    return errors['required']
+      ? 'Password is required'
+      : 'Password has not a vaild structure';
   }
 
   onSubmit() {
     // if form is invalid, console log error.
+    if (this.loginForm.valid) console.log('Congrats - valid form');
+    if (!this.loginForm.valid)
+      console.error('Sorry :( invalid form. Try again.');
+
     console.log('form: ', this.loginForm);
     const userEmail = this.loginForm.value.email;
     const userPassword = this.loginForm.value.password;
