14e8c3c52f6f21922e4d859c1f9975eb70520374
klarutkow <86961229+klarutkow@users.noreply.github.com>
2025-01-20 11:32:39 +0100

    Password validation via RegEx

--- frontend-app/src/app/components/auth-form/auth-form.component.ts
+++ frontend-app/src/app/components/auth-form/auth-form.component.ts
@@ -5,6 +5,9 @@ import {
   FormGroup,
   ReactiveFormsModule,
   Validators,
+  AbstractControl,
+  ValidatorFn,
+  ValidationErrors,
 } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 
@@ -21,13 +24,35 @@ export class AuthFormComponent {
       validators: [Validators.required, Validators.email],
     }),
     password: new FormControl('', {
-      validators: [Validators.required],
+      validators: [Validators.required, this.passwordValidator()],
     }),
   });
 
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.value;
+
+      // RegEx dla wymagań hasła:
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+      // Sprawdzanie hasła z wykorzystaniem RegEx
+      const valid = passwordRegex.test(password);
+
+      return valid ? null : { passwordInvalid: true };
+    };
+  }
+
   isEmailInvalid(): boolean | undefined {
     console.log(this.loginForm.get('email')?.errors);
-    return this.loginForm.get('email')?.invalid;
+    return (
+      this.loginForm.get('email')?.invalid &&
+      this.loginForm.get('email')?.touched
+    );
   }
 
   getEmailErrorMessage(): string {
@@ -42,24 +67,47 @@ export class AuthFormComponent {
 
   isPasswordInvalid(): boolean | undefined {
     console.log(this.loginForm.get('password')?.errors);
-    return this.loginForm.get('password')?.invalid;
+    return (
+      this.loginForm.get('password')?.invalid &&
+      this.loginForm.get('password')?.touched
+    );
   }
 
   getPasswordErrorMessage(): string {
-    const errors = this.loginForm.get('password')?.errors;
+    const errors = this.loginForm.get('password')?.errors?.['passwordInvalid'];
+    const passwordRequiredError =
+      this.loginForm.get('password')?.errors?.['required'];
+
     if (!errors) {
       return '';
     }
-    return errors['required']
-      ? 'Password is required'
-      : 'Password has not a vaild structure';
+
+    if (passwordRequiredError) {
+      return 'Password is required';
+    }
+
+    return errors['passwordInvalid']
+      ? ''
+      : 'The password must contain at least one uppercase letter, one lowercase letter, one number, one special character and be at least 8 characters long.';
   }
+
+  // getPasswordErrorMessage(): string {
+  //   const errors = this.loginForm.get('password')?.errors;
+  //   if (!errors) {
+  //     return '';
+  //   }
+  //   return errors['required']
+  //     ? 'Password is required'
+  //     : 'Password has not a vaild structure';
+  // }
 
   onSubmit() {
     // if form is invalid, console log error.
     if (this.loginForm.valid) console.log('Congrats - valid form');
-    if (!this.loginForm.valid)
-      console.error('Sorry :( invalid form. Try again.');
+    if (!this.loginForm.valid) {
+      console.error('Sorry :( Invalid form. Try again.');
+      this.getPasswordErrorMessage();
+    }
 
     console.log('form: ', this.loginForm);
     const userEmail = this.loginForm.value.email;
