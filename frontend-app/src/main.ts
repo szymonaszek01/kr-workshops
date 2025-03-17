da49fefa2f27e8e5696e1f3b29834558b6a538ee
klarutkow <86961229+klarutkow@users.noreply.github.com>
2025-03-17 18:25:07 +0100

    Add Interceptor and Guard

--- frontend-app/src/main.ts
+++ frontend-app/src/main.ts
@@ -1,14 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { AuthInterceptor } from './app/interceptor/auth-interceptor';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), // required animations providers
+    provideToastr({
+      timeOut: 8000,
+      positionClass: 'toast-bottom-right',
+    }), // Toastr providers
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 }).catch((err) => console.error(err));
