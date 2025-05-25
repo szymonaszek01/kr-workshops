bafdc2b1fef0d07210870344345f6e09164aae38
szymonaszek01 <235879@edu.p.lodz.pl>
2025-05-25 18:40:39 +0200

    expose backend api to refresh token

--- backend-app/routes/auth-router.js
+++ backend-app/routes/auth-router.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { login } = require("../services/auth-service");
+const { login, refreshToken } = require("../services/auth-service");
 
 const authRouter = express.Router();
 
 authRouter.post("/auth/login", async (req, res) => {
   await login(req.body)
     .then((authUser) => res.json(authUser))
+    .catch((error) => res.status(403).send({ error }));
+});
+
+authRouter.post("/auth/refresh-token", async (req, res) => {
+  const { token } = req.body;
+  await refreshToken(token)
+    .then((token) => res.json(token))
     .catch((error) => res.status(403).send({ error }));
 });
 
